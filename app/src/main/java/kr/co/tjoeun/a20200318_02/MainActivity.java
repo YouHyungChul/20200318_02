9fd99bdc6b4f5e8eac6cfb93a1739d70c78df376
YouHyungChul <61854958+YouHyungChul@users.noreply.github.com>
2020-03-18 11:04:14 +0900

    데이터 클래스 통째로 전달하는 예시

--- app/src/main/java/kr/co/tjoeun/a20200318_02/MainActivity.java
+++ app/src/main/java/kr/co/tjoeun/a20200318_02/MainActivity.java
@@ -2,7 +2,11 @@ package kr.co.tjoeun.a20200318_02;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import kr.co.tjoeun.a20200318_02.datas.User;
+
 //부모를 바꾸고 난뒤 빨간색 뜨면 ALT + ENter (에러줄에서)
 public class MainActivity extends BaseActivity {
 
@@ -25,6 +29,16 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void setupEvents() {
+
+//        Intent로 UserInfoActivity 실행.
+//        사용자 이름 / 생년 전달.   +  성별 / 잔고 / 아이디 / 닉네임까지 전달해볼까
+//        ==> User 클래스를 한번에 전달하면 편하다!
+//        데이터 전달을 간으하게 하려면, 해당 클래스 Serializable 역활 수행 명시.
+        User u = new User("조경진", 1988);
+
+        Intent intent = new Intent(MainActivity.this, UserInfoActivity.class);
+        intent.putExtra("userInfo","이름");
+
 
     }
 
---
+++ app/src/main/java/kr/co/tjoeun/a20200318_02/UserInfoActivity.java
@@ -0,0 +1,14 @@
+package kr.co.tjoeun.a20200318_02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_info);
+    }
+}
